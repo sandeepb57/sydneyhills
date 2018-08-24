76bf571104c97523f1de5b44a8b209fcaab7dfc5
Jilanisk08 <38452385+Jilanisk08@users.noreply.github.com>
2018-08-24 12:49:28 +0530

    dummy payment gateway ui added

--- application/views/ViewPayments.php
+++ application/views/ViewPayments.php
@@ -21,39 +21,210 @@
         <meta property="og:title" content="Horse Riding Lessons, School Holiday Camps, Pony Parties: Expert Coaches"/>
         <meta property="og:image" content=""/>
 
-        <?php $this->load->view("imports-top");?>
+        <?php $this->load->view("imports-top"); ?>
+        <style nonce="">
+
+            .logo {
+                width: 112px;
+                height: 29px;
+                margin: 5px 0 20px;
+                background-position: 0 -47px;
+                background-image: url(https://www.paypalobjects.com/images/checkout/hermes/hermes_window_sprite_v16.png);
+                background-repeat: no-repeat;
+            }
+            .main {
+                padding:30px;
+                margin: 0 auto;
+                width: 780px;
+                background-color: #fff;
+                text-align: center;
+                margin-top: 90px;
+                color: #7b8388;
+                font-family: "Helvetica Neue", "HelveticaNeue", "HelveticaNeue-Light", "Helvetica Neue Light", helvetica, arial, sans-serif;
+                font-size: 95%;
+                line-height: 1.35em;
+                -webkit-font-smoothing: antialiased;
+            }
+
+            .content-wrap {
+                text-align: left;
+                border: 1px solid #e3e3e3;
+                border-radius: 5px;
+                box-sizing: border-box;
+            }
+
+            .message {
+                font-size: 1.0em;
+                margin-bottom: 15px;
+            }
+
+
+
+            .right {
+                float: right;
+                text-align: right;
+            }
+
+            .footerLinks {
+                list-style: none;
+                margin: 0;
+                padding: 0;
+            }
+
+            .footerLinks li {
+                padding: 0 12px 0 0;
+                float: left;
+            }
+
+            .footerLinks a {
+                text-decoration: none;
+                -webkit-tap-highlight-color: rgba(0, 0, 0, 0.2);
+                color: #6C7378;
+            }
+
+            .copyright {
+                vertical-align: top;
+                color: #6C7378;
+            }
+
+            .secureIcon {
+                display: inline-block;
+                background-image: url(https://www.paypalobjects.com/images/checkout/hermes/hermes_window_sprite_v16.png);
+                background-repeat: no-repeat;
+                background-position: 0 -33px;
+                margin-left: 3px;
+                margin-right: 3px;
+                margin-top: 3px;
+                width: 10px;
+                height: 12px;
+            }
+
+            .btn {
+                height: 44px;
+                margin-bottom: 0;
+                border-style: solid;
+                border-width: 1px;
+                border-color: #0070BA;
+                -webkit-border-radius: 5px;
+                -moz-border-radius: 5px;
+                border-radius: 5px;
+                *margin-left: .3em;
+                display: inline-block;
+                /* IE7 inline-block hack */
+                *display: inline;
+                *zoom: 1;
+                background-color: #0070BA;
+                transition:background-color 0.4s ease-out 0s;
+                text-align: center;
+                vertical-align: middle;
+                cursor: pointer;
+                outline: none;
+                -webkit-font-smoothing: antialiased;
+                width: 100%;
+            }
+
+            .btn a {
+                line-height: 42px;
+                text-decoration : none;
+                color: white;
+                font-size: 1.0em;
+                font-weight: bold;
+            }
+            #papypal-wrapper input{
+                border:  1px solid #9e9e9e;
+                border-radius: 4px;
+                outline: none;
+                height: 45px;
+                width: 100%;
+                font-weight: 500;
+                padding-left: 1rem;
+                margin-bottom: 10px;
+            }
+            #papypal-wrapper select{
+                border:  1px solid #9e9e9e;
+                border-radius: 4px;
+                outline: none;
+                height: 45px;
+                width: 100%;
+                font-size: 14px;
+                margin: 0 0 8px 0;
+                padding: 0;
+                font-weight: 500;
+                padding-left: 1rem;
+            }
+
+        </style>
     </head>
     <body>
         <div id="fh5co-wrapper">
             <div id="fh5co-page">
+                <?php $this->load->view("header"); ?>
+                <main>
+                    <div class="container">
+                        <div class="row">
+                            <div id="booking-summary">
+                                <div class="main" id="papypal-wrapper">
+                                    <div class="panel panel-default bg-white">
+                                        <div class="panel-header">
+                                            <div class="panel-heading">
+                                                <div class="logo"></div>
+                                            </div>
+                                        </div>
+                                        <div class="panel-body">
+                                            <div class="col-md-7">
+                                                <div class="paypal-heading">
+                                                    <h1 class="title">Paypal Checkout</h1>
+                                                    <p class="text-left m-0">we don't share  your financial details  with the merchant</p>
+                                                </div>
+                                                <div class="content">
+                                                    <form>
+                                                        <div class="input-field col-xxs-12 col-xs-12 mt-5 mb-3">
+                                                            <select name="country" class="country">
+                                                                <option value="">Select Country </option>
+                                                                <option value="1">India</option>
+                                                            </select>
+                                                        </div>
+                                                        <div class="input-field col-xxs-12 col-xs-12 mb-3">
+                                                            <input type="text" placeholder="card number" >
+                                                        </div>
+                                                        <div class="input-field col-xxs-12 col-xs-6 mb-3">
+                                                            <input type="text" placeholder="expires" >
+                                                        </div>
 
-				<?php $this->load->view("header");?>
+                                                        <div class="input-field col-xxs-12 col-xs-6 mb-3">
+                                                            <input type="text" placeholder="cvv" >
+                                                        </div>
+                                                        <div class="input-field col-xxs-12 col-xs-6 mb-3">
+                                                            <input type="text" placeholder="First Name" >
+                                                        </div>
 
-				<main>
-					<div class="container">
-						<div class="row  mt-40 mb-40">
-							<div id="booking-summary">
-								<div class="col-xs-10 col-xs-push-1 col-sm-8 col-sm-push-3 col-md-8 col-md-push-2">
-									<div class="row mb-30">
-										<h1 class="text-black">Select payment method</h1>
-									</div>
-								</div>
-								<?php // if ($status == 200) {?>
-									<div class="col-xs-10 col-xs-push-1 col-sm-8 col-sm-push-3  col-md-8 col-md-push-2 p-0">
-										<a class="btn btn-primary btn-block" href="<?php echo base_url(); ?>BookHorses/ProceedToPay?bookingId=<?php echo $bookingId; ?>">Proceed to pay</a>
-									</div>
-								<?php // }?>
-							</div>
-						</div>
-					</div>
-				</main>
+                                                        <div class="input-field col-xxs-12 col-xs-6 mb-3">
+                                                            <input type="text" placeholder="Last Name" >
+                                                        </div>
 
-				<?php $this->load->view("footer");?>
 
-    		</div>
-		</div>
+                                                        <?php // if ($status == 200) {?>
+                                                        <div class="col-xxs-12 col-xs-12 mb-3">
+                                                            <a class="btn btn-primary btn-paypal btn-block" href="<?php echo base_url(); ?>BookHorses/ProceedToPay?bookingId=<?php echo $bookingId; ?>">Proceed to pay</a>
+                                                        </div>
+                                                        <?php // }?>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-5">
+                                                <img class="img-responsive" src="<?php echo base_url(); ?>assets/images/paypal_bg.jpg" alt="" />
+                                            </div>
+                                        </div>
 
-		<?php $this->load->view("imports-bottom");?>
-
-	</body>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+                <?php $this->load->view("footer"); ?>
+            </div>
+        </div>
+        <?php $this->load->view("imports-bottom"); ?>
+    </body>
 </html>
